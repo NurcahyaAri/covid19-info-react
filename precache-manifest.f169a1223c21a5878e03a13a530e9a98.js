11b994c3ab4db2c3d379a144583eb74373062392
Ari Nurcahya <nurcahyaari@gmail.com>
2020-03-15 01:41:38 +0700

    Updates

---
+++ precache-manifest.f169a1223c21a5878e03a13a530e9a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa5effed7061bc32333c6cd0bab7e3f",
+    "url": "/covid19-info/index.html"
+  },
+  {
+    "revision": "8ed76c72c5c63face226",
+    "url": "/covid19-info/static/css/2.4e870e1d.chunk.css"
+  },
+  {
+    "revision": "8ed76c72c5c63face226",
+    "url": "/covid19-info/static/js/2.9f197152.chunk.js"
+  },
+  {
+    "revision": "995084f4dd276fa9cc26db82da00db86",
+    "url": "/covid19-info/static/js/2.9f197152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd83a20eb13ee6c52842",
+    "url": "/covid19-info/static/js/main.5aba96eb.chunk.js"
+  },
+  {
+    "revision": "ff28e1285e64d46c3ffa",
+    "url": "/covid19-info/static/js/runtime-main.5a642bb7.js"
+  }
+]);
